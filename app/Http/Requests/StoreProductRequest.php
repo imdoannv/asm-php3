257cb61a8e4b751d333cf524fba89c069a96e1d7
Quang Phuc <124752859+qphucc501@users.noreply.github.com>
2023-07-19 15:23:24 +0700

    Product

---
+++ app/Http/Requests/StoreProductRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        $tableName = (new Product()) ->getTable();
+        return [
+            'name' => "required|string|max:50|unique:$tableName,name",
+            'describe' => ['nullable','string','max: 200'],
+            'price'=>['required','numeric','min:100'],
+        ];
+    }
+}
